c8e54f0aae416a2a42414f7549619ef93a62eb25
Al3xandr <Al3xandr.Goryunov@yandex.ru>
2025-03-17 11:21:58 +0300

    Initial Commit

---
+++ MoonshinerAndBrewerCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoonshinerAndBrewerCalculator
+//
+//  Created by Alexandr on 17.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
